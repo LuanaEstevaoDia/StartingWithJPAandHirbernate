4adc90719ca98a04041214df57c4864664a800f4
LuanaEstevaoDia <luanafreitas.ti@gmail.com>
2024-08-16 10:46:35 -0300

    Search by Id in the 'person' table

--- inicializandojpamaven/src/main/java/Aplicacao/Programa.java
+++ inicializandojpamaven/src/main/java/Aplicacao/Programa.java
@@ -9,21 +9,18 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
+		/**busca não precisa do transaction.*/
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+	
 
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-
-		em.getTransaction().commit();
-		System.out.println("Congratulations, your table has been created and your data inserted.");
-
+		
+		em.close();
+		emf.close();
 	}
 
 }
